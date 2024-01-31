e3f008a6b7fe9a1921a3a27162ef6b5318881d95
Henri_Jantsch <139029539+henrijantsch@users.noreply.github.com>
2024-01-31 15:06:13 -0300

    plugins_jquery_revisao

--- plugins_jquery/js/main.js
+++ plugins_jquery/js/main.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
         autoplay: true
     });
     
+    
     $('.menu-hamburguer').click(function() {
         $('nav').slideToggle();
     })
 
-    $('#telefone').mask('(00)00000-0000')
+    $('#telefone').mask('(00)00000-0000');
+    $('#cep').mask('00000-000');
+    $('#cpf').mask('000.000.000-00');
+    
+    
 
     $('form').validate({
         rules: {
@@ -37,4 +42,8 @@ $(document).ready(function() {
             scrollTop: destino.offset().top
         }, 1000)
     })
+    
 })
+
+
+
